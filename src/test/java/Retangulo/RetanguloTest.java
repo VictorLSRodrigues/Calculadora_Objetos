299c6fc2cf50daeddf7feaee13a809c0e670131b
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-04-15 10:28:24 -0300

    fix format Calculadora and CalculadoraTest and principal

---
+++ src/test/java/Retangulo/RetanguloTest.java
@@ -0,0 +1,18 @@
+package Retangulo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RetanguloTest {
+
+    @Test
+    void testGetArea() {
+        double altura = 3;
+        double base = 2;
+        double retornoEsperado = 6;
+        Retangulo retangulo = new Retangulo("", base, altura);
+        double retornoFeito = retangulo.getArea();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+}
---
+++ src/test/java/figuras/FiguraTest.java
@@ -0,0 +1,16 @@
+package figuras;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import Retangulo.Retangulo;
+
+public class FiguraTest {
+
+    @Test
+    void testGetCor() {
+        String retornoEsperado = "Verde";
+        Figura retangulo = new Retangulo();
+        retangulo.setCor("Verde");
+        assertEquals(retornoEsperado, retangulo.getCor());
+    }
+}
